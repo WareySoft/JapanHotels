3a785da4ec6f2e1766b74531fad9f81aa12e4ff7
WareySoft <softerwarey@gmail.com>
2022-02-17 14:05:24 +0600

    scroll view

--- main/index.js
+++ main/index.js
@@ -1,3 +1,14 @@
+function myFunction() {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 8000);
+}
+
+
+$(document).ready(myFunction());
+
+
+
 var audio = new Audio('eternal.mp3');
 
 var x = 0;
@@ -14,3 +25,84 @@ $("#player").click(function() {
   x++;
 });
 
+$(document).ready(function () { 
+    
+   $('#myDiv').animate({ 
+            height: '100%', 
+            width: '1200px' 
+        }, 
+        1000); 
+});
+ 
+
+
+
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const square = entry.target.querySelector('.squared');
+
+    if (entry.isIntersecting) {
+      square.classList.add('square-animation');
+	  return; // if we added the class, exit the function
+    }
+
+    // // We're not intersecting, so remove the class!
+    // square.classList.remove('square-animation');
+  });
+});
+
+observer.observe(document.querySelector('.square-wrapper1'));
+observer.observe(document.querySelector('.square-wrapper2'));
+observer.observe(document.querySelector('.square-wrapper3'));
+observer.observe(document.querySelector('.review1'));
+observer.observe(document.querySelector('#wave1'));
+observer.observe(document.querySelector('#wave2'));
+
+
+const observer2 = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const square = entry.target.querySelector('.imaged');
+
+    if (entry.isIntersecting) {
+      square.classList.add('image-animation');
+	  return; // if we added the class, exit the function
+    }
+
+    // // We're not intersecting, so remove the class!
+    // square.classList.remove('image-animation');
+  });
+});
+
+observer2.observe(document.querySelector('#images'));
+observer2.observe(document.querySelector('#player'));
+
+
+
+
+
+const observer3 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('square-animation2');
+    }
+  });
+});
+
+observer3.observe(document.querySelector('#wave2'));
+observer3.observe(document.querySelector('.findhotelt'));
+
+
+
+
+
+observer3.observe(document.querySelector('#navt1'));
+observer3.observe(document.querySelector('#navt2'));
+observer3.observe(document.querySelector('#navt3'));
+observer3.observe(document.querySelector('#navt4'));
+observer3.observe(document.querySelector('#player'));
+observer3.observe(document.querySelector('.textcity'));
+observer3.observe(document.querySelector('.directions'));
