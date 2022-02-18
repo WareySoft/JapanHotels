6a3ba521108da0a849310b3f55b18da283443eeb
WareySoft <softerwarey@gmail.com>
2022-02-19 03:13:38 +0600

    main page completed

--- main/index.js
+++ main/index.js
@@ -4,8 +4,37 @@ function myFunction() {
   setTimeout(function(){ x.className = x.className.replace("show", ""); }, 8000);
 }
 
-
+ 
 $(document).ready(myFunction());
+
+
+$("#searchbut").click(function() {
+  navigate()
+});
+
+function navigate(){
+  var getValue = document.getElementById('myselect').value; 
+  if(getValue == "tokyo"){
+    window.open('https://www.kayak.com/Tokyo-Hotels.21033.hotel.ksp?r9ck=iq&gclid=Cj0KCQiApL2QBhC8ARIsAGMm-KEVUgJIpDRm7RG27EuKKXCZIbGeG9sF8CGrLqmITzfzfwZEzIBynFUaAlOmEALw_wcB');
+    location.href='../tokyo/tokyohtml.html';  
+  }
+  else if(getValue == "tosaka"){
+    window.open('https://www.kayak.com/Osaka-Hotels.25901.hotel.ksp?r9ck=iq&gclid=Cj0KCQiApL2QBhC8ARIsAGMm-KExweKpHja8D0DoNgwViNDmDpxVxCfQiIXb36mvGy2OiyBRtWIxZoYaAi1QEALw_wcB');
+    location.href='../tosaka/tosakahtml.html';  
+  }
+  else if(getValue == "niigata"){
+    window.open('https://www.kayak.com/Niigata-Hotels.11087.hotel.ksp?r9ck=iq&gclid=Cj0KCQiApL2QBhC8ARIsAGMm-KHHcKdP9KEPawCtYrqWHVUNg7NigfwkIkaYjtyjT6S9EOy_-E6w7q8aAnAuEALw_wcB');
+    location.href='../niigata/nigatahtml.html';
+  }
+  else if(getValue == "kyoto"){
+    window.open('https://www.kayak.com/Kyoto-Hotels.20339.hotel.ksp?r9ck=iq&gclid=Cj0KCQiApL2QBhC8ARIsAGMm-KEQNogOrirC8nqb0coZTK0GMVK4IrStQSc8hmbG_ERn6u6QI7fIfaoaAtLlEALw_wcB');
+    location.href='../kyoto/kyotohtml.html';
+  } 
+  console.log(getValue);
+}
+
+
+
 
 
 
@@ -16,11 +45,11 @@ var animation = document.querySelector("div");
 
 $("#player").click(function() {
   if(x%2 == 0){
-  	audio.play();
-  	animation.id = "leaves";
+    audio.play();
+    animation.id = "leaves";
   }else{
-  	audio.pause();
-  	animation.id = "";
+    audio.pause();
+    animation.id = "";
   }
   x++;
 });
@@ -41,6 +70,24 @@ $("#player").click(function() {
  
 
 
+$(document).ready(function () {
+    $("#myselect").change(function () {
+        var val = $(this).val();
+        if (val == "tokyo") {
+            $("#selecthotel").html("<option value='test'>Sunroute Plaza</option><option value='test2'>Sinjuku Granbellh</option>");
+        } else if (val == "tosaka") {
+            $("#selecthotel").html("<option value='test'>Hiyori</option><option value='test2'>Sliber</option>");
+        } else if (val == "niigata") {
+            $("#selecthotel").html("<option value='test'>Okura</option><option value='test2'>Furumachi</option>");
+        } else if (val == "kyoto") {
+            $("#selecthotel").html("<option value=''>Hotel Granvia</option><option value=''>Mystays</option>");
+        }
+    });
+});
+
+
+
+
 
 
 const observer = new IntersectionObserver(entries => {
@@ -49,7 +96,7 @@ const observer = new IntersectionObserver(entries => {
 
     if (entry.isIntersecting) {
       square.classList.add('square-animation');
-	  return; // if we added the class, exit the function
+    return; // if we added the class, exit the function
     }
 
     // // We're not intersecting, so remove the class!
@@ -71,7 +118,7 @@ const observer2 = new IntersectionObserver(entries => {
 
     if (entry.isIntersecting) {
       square.classList.add('image-animation');
-	  return; // if we added the class, exit the function
+    return; // if we added the class, exit the function
     }
 
     // // We're not intersecting, so remove the class!
